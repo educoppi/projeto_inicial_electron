a05c367fbb89cb9a29643110bf4df1000b41c415
educoppi <eduardo.bcoppi@senacsp.edu.br>
2026-02-04 17:00:13 -0300

    chore: Criado botao enviar mensagem

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { app, BrowserWindow } from 'electron';
 
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600
-    });
-    win.loadFile('./src/index.html');
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: './src/preload.ts',
+    },
+  });
+  win.loadFile('./src/index.html');
 }
 
 app.whenReady().then(createWindow);
