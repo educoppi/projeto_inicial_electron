807eca66453fb9a59be55606e62e3259dbec0618
educoppi <eduardo.bcoppi@senacsp.edu.br>
2026-02-04 14:38:46 -0300

    chore: cria base do electron hello world

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { app, BrowserWindow } from 'electron';
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+    win.loadFile('./src/index.html');
+}
+
+app.whenReady().then(createWindow);
